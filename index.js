62a91c866f9906b6159d97e8a503ba0cedb55178
Ihor Bondarenko <ihor.bondarenko@acropolium.com>
2017-03-06 19:07:37 +0200

    new files

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var fs = require('fs'),
-    xml2js = require('xml2js');
+    xml2js = require('xml2js'),
+    _ = require('lodash');
 
 var parser = new xml2js.Parser();
 fs.readFile(__dirname + '/CAP.xml', function(err, data) {
     parser.parseString(data, function (err, result) {
-        console.dir(result);
-        console.log('Done');
+        if(_.isObject(result) && _.has(result,'alert') && _.isObject(result['alert'])){
+            if(_.has(result['alert'],'info') && _.isObject(result['alert']['info'])){
+                _.forEach(result['alert']['info'],function(v,k){
+                        if(_.isObject(v)){
+                            _.forEach(v,function(v2,k2){
+                                console.log(k2+ ' => ' +v2)
+                            });
+                        }
+                });
+            }
+        }
     });
 });
